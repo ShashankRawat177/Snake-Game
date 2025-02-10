27d58863008153267a02ead2199de366b3f86a35
ShashankRawat177 <shashankrawat177@gmail.com>
2025-02-10 22:25:54 +0530

    Update header.h

--- header.h
+++ header.h
@@ -1,1 +1,40 @@
+#ifndef SNAKEGAME_H
+#define SNAKEGAME_H
 
+#include <iostream>
+#include <random>
+#include <thread>
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+class SnakeGame {
+private:
+    bool isGameOver;
+    int x, y, fruitx, fruity, specialAx, specialAy, specialBx, specialBy, Score;
+    bool hasSpecialA, hasSpecialB;
+    const int GridLength = 20;
+    const int GridWidth = 20;
+    int TailX[400], TailY[400];
+    int nTail;
+    enum Direction{STOP = 0, UP, DOWN, LEFT, RIGHT};
+    Direction dir;
+    std::mt19937 rng;  // Mersenne Twister RNG
+    std::uniform_int_distribution<int> distX, distY, distChance;  // Distributions
+
+public:
+    SnakeGame();
+    void gotoxy(int x, int y);
+    void hideCursor();
+    void showCursor();
+    void spawnFruit();
+    void startup();
+    void MainMenu();
+    void draw();
+    void input();
+    void logic();
+    void run();
+};
+
+#endif // SNAKEGAME_H
